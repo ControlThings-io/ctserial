16b3dccfe0acb4208ecc8003f6c4eb2c6ba2de53
Justin Searle <justin@controlthings.io>
2021-04-22 15:49:55 -0600

    Initial changes for ctui 0.7 compatibility

--- src/ctserial/commands.py
+++ src/ctserial/commands.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2018  Justin Searle
+# Copyright (C) 2021  Justin Searle
 #
 # This program is free software: you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free Software
@@ -9,175 +9,144 @@
 # FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
 # details at <http://www.gnu.org/licenses/>.
 
-import shlex
 import re
-import serial
-import serial.tools.list_ports
-import time
+import shlex
+from datetime import datetime
 
-from ctui.application import Ctui
+from ctui import Ctui
 from ctui.dialogs import message_dialog
+from pkg_resources import get_distribution
+from serial import (
+    EIGHTBITS,
+    PARITY_EVEN,
+    PARITY_MARK,
+    PARITY_NONE,
+    PARITY_ODD,
+    PARITY_SPACE,
+    STOPBITS_ONE,
+    Serial,
+)
 from tabulate import tabulate
 
+from ctserial import common
 
-class CtSerial(Ctui):
-    """Commands that users may use at the application prompt."""
-    name = 'ctserial'
-    version = '0.5'
-    description = 'a security professional\'s swiss army knife for interacting with serial devices'
-    prompt = 'ctserial> '
-    session = None
-    unit_id = 1
-    statusbar = 'Session:{}'.format(session)
-    session = ''
-    output_format = 'mixed'
-    macro_hex = {}
-
-
-    # def get_statusbar_text():
-    #     sep = '  -  '
-    #     session = get_app().session
-    #     if type(session) == serial.Serial:
-    #         device = 'connected:' + session.port
-    #     else:
-    #         device = 'connected:None'
-    #     output_format = 'output:' + get_app().output_format
-    #     return sep.join([device, output_format])
+ctserial = Ctui()
+ctserial.name = "ctserial"
+ctserial.version = get_distribution("ctserial").version
+ctserial.description = (
+    "a security professional's swiss army knife for interacting with serial devices"
+)
+ctserial.prompt = "ctserial> "
+ctserial.session = None
+# ctserial.statusbar = f"Session:{ctserial.session.port}"
+ctserial.output_format = "mixed"
+ctserial.macros = {}
 
 
-    def do_connect(self, args, output_text):
-        """Generate a session with a single serial device to interact with it."""
-        parts = args.split()
-        devices = [x.device for x in serial.tools.list_ports.comports()]
-        if len(parts) > 0:
-            device = parts[0]
-            if len(parts) > 1:
-                baudrate = parts[1]
-            else:
-                baudrate = 9600
-            if device in devices:
-                self.session = serial.Serial(
-                    port=device,
-                    baudrate=baudrate,
-                    parity=serial.PARITY_NONE,
-                    stopbits=serial.STOPBITS_ONE,
-                    bytesize=serial.EIGHTBITS)
-                # initiate a serial session and return success message
-                self.session.isOpen()
-                output_text += 'Connect session opened with {}\n'.format(device)
-                return output_text
-        # return list of devices if command incomplete or incorrect
-        message = 'Valid devices:\n' + ', \n'.join(devices) + '\n'
-        message_dialog(title='Invalid Device', text=message)
-        return False
+@ctserial.command
+def do_connect(device: str = "/dev/null/", baudrate: int = 9600, parity=PARITY_NONE):
+    """
+    Connect to a serial device
+
+    :PARAM: device: Serial device path or COM port
+    :PARAM: baudrate: Defaults to 9600. Most standard rates from 75 to 4000000 supported.
+    :PARAM: parity: Defaults to none. Can also be even, odd, mark, or space
+    """
+    assert (
+        ctserial.session == None
+    ), "Session already open.  Close first."  # ToDo assert session type
+    valid_device = common.validate_serial_device(device)
+    parities = {
+        "none": PARITY_NONE,
+        "even": PARITY_EVEN,
+        "odd": PARITY_ODD,
+        "mark": PARITY_MARK,
+        "space": PARITY_SPACE,
+    }
+    assert (
+        parity in parities.keys()
+    ), "Parity must be none (default), even, odd, mark, or space"
+    s = Serial(
+        port=valid_device,
+        baudrate=baudrate,
+        parity=parities[parity],
+        stopbits=STOPBITS_ONE,
+        bytesize=EIGHTBITS,
+    )
+    assert s.isOpen(), f"Could not connect to {valid_device}"
+    ctserial.session = s
+    date, time = str(datetime.today()).split()
+    return (
+        ctserial.output_text
+        + f"{date} {time} - ASCII session OPENED with {valid_device}\n"
+    )
 
 
-    def do_close(self, args, output_text):
-        """Close a session."""
-        if type(self.session) != serial.Serial:
-            output_text += 'Connect to a device first\n'
-            return output_text
-        else:
-            device = self.session.port
-            self.session.close()
-            output_text += 'Session with {} closed.'.format(device) + '\n'
-            self.session = ''
-        return output_text
+@ctserial.command
+def do_close():
+    """
+    Close the open session
+    """
+    assert (
+        ctserial.session
+    ), "There is not an open session.  Connect to one first."  # ToDo assert session type
+    ctserial.session.close()
+    ctserial.session = None
+    date, time = str(datetime.today()).split()
+    return (
+        ctserial.output_text
+        + f"{date} {time} - Session with {ctserial.session.port} CLOSED\n"
+    )
 
 
-    def _send_instruction(self, session, tx_bytes):
-        """Send data to serial device"""
-        # clear out any leftover data
-        try:
-            rx_raw = bytes()
-            if session.inWaiting() > 0:
-                session.flushInput()
-            session.write(tx_bytes)
-            time.sleep(0.1)
-            while session.inWaiting() > 0:
-                rx_raw += session.read()
-        except BaseException as e:
-            output = '\n\n{}'.format(e)
-        time.sleep(0.1)
-        return rx_raw
+@ctserial.command
+def do_send():
+    """Various send functions..."""
 
 
-    def _format_output(self, raw_bytes, prefix=''):
-        """ Return hex and utf-8 decodes aligned on two lines """
-        if len(raw_bytes) == 0:
-            return prefix + 'None'
-        table = []
-        if self.output_format == 'hex' or self.output_format == 'mixed':
-            hex_out = [prefix] + list(bytes([x]).hex() for x in raw_bytes)
-            table.append(hex_out)
-        if self.output_format == 'ascii' or self.output_format == 'mixed':
-            ascii_out = [' ' * len(prefix)] + list(raw_bytes.decode('ascii', 'replace'))
-            table.append(ascii_out)
-        if self.output_format == 'utf-8':
-            # TODO: track \xefbfdb and replace with actual sent character
-            utf8 = raw_bytes.decode('utf-8', 'replace')
-            utf8_hex_out = [prefix] + list(x.encode('utf-8').hex() for x in utf8)
-            utf8_str_out = [' ' * len(prefix)] + list(utf8)
-            table = [utf8_hex_out, utf8_str_out]
-        return tabulate(table, tablefmt="plain", stralign='right')
+@ctserial.command
+def do_send_hex(data: str):
+    """
+    Send raw hex to serial device, ignoring spaces \\x and 0x.
+
+    :PARAM: data: Raw hex to send.
+    """
+    assert (
+        ctserial.session
+    ), "There is not an open session.  Connect to one first."  # ToDo assert session type
+    raw_hex = data.lower().replace("0x", "").replace("\\x", "").replace(" ", "")
+    assert re.match("^[0123456789abcdef]+$", raw_hex), "Only hex characters allowed"
+    assert len(raw_hex) % 2 != 0, "You must send an even number of hex characters"
+    tx_bytes = bytes.fromhex(raw_hex)
+    rx_bytes = common.send_instruction(ctserial.session, tx_bytes)
+    output_text = ctserial.output_text
+    output_text += common.format_output(tx_bytes, "--> ", ctserial.output_format) + "\n"
+    output_text += common.format_output(rx_bytes, "<-- ", ctserial.output_format) + "\n"
+    return output_text
 
 
-    def do_sendhex(self, args, output_text):
-        """Send raw hex to serial device."""
-        if type(self.session) != serial.Serial:
-            output_text += 'Connect to a device first\n'
-            return output_text
-        data = args.lower().replace("0x", "")
-        if re.match('^[0123456789abcdef\\\\x ]+$', data):
-            raw_hex = re.sub('[\\\\x ]', '', data)
-            if len(raw_hex) % 2 == 0:
-                tx_bytes = bytes.fromhex(raw_hex)
-                session = self.session
-                rx_bytes = self._send_instruction(session, tx_bytes)
-                output_text += self._format_output(tx_bytes, prefix='--> ') + '\n'
-                output_text += self._format_output(rx_bytes, prefix='<-- ') + '\n'
-                return output_text
-        return False
+@ctserial.command
+def do_send_utf8(data: str):
+    """
+    Send UTF-8 string to serial device.
 
-
-    def do_send(self, args, output_text):
-        """Send string to serial device."""
-        if type(self.session) != serial.Serial:
-            output_text += 'Connect to a device first\n'
-            return output_text
-        if len(args) > 0:
-            # remove spaces not in quotes and format
-            string = ''.join(shlex.split(args))
-            tx_bytes = bytes(string, encoding='utf-8')
-            session = self.session
-            rx_bytes = self._send_instruction(session, tx_bytes)
-            output_text += self._format_output(tx_bytes, prefix='--> ') + '\n'
-            output_text += self._format_output(rx_bytes, prefix='<-- ') + '\n'
-            return output_text
-        return False
-
-
-    def do_macro_set(self, args, output_text):
-        v1 = args[: args.find(' ')]
-        v2 = args[args.find(' '): ]
-        self.macro_hex[v1] = v2
-        output_text += "key " + v1 + " set to value " + v2 + "\n"
-        return output_text
-
-
-    def do_macro_send(self, args, output_text):
-        macro = self.macro_hex[args]
-        if macro:
-            return self.do_sendhex(macro, output_text)
-        output_text += 'Unknown macro\n'
-        return output_text
-
-
+    :PARAM: data: UTF-8 string to send.
+    """
+    assert (
+        ctserial.session
+    ), "There is not an open session.  Connect to one first."  # ToDo assert session type
+    utf8_str = "".join(shlex.split(data))  # remove spaces not in quotes and format
+    tx_bytes = bytes(utf8_str, encoding="utf-8")
+    rx_bytes = common.send_instruction(ctserial.session, tx_bytes)
+    output_text = ctserial.output_text
+    output_text += common.format_output(tx_bytes, "--> ", ctserial.output_format) + "\n"
+    output_text += common.format_output(rx_bytes, "<-- ", ctserial.output_format) + "\n"
+    return output_text
 
 
 def main():
-    ctserial = CtSerial()
     ctserial.run()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
